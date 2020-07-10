375d70d0ff0bd51784b3bb35a25a1e8007a66360
KY1212 <YumYum1212@docomo.ne.jp>
2020-07-10 12:30:06 +0900

    add カレンダーの追加、日付クリックでその日に応じたjsonデータを表示させる処理の追加

--- js/practice.js
+++ js/practice.js
@@ -42,10 +42,106 @@ $(function () {
   $(document).ready(function () {
     $.getJSON("data/sample_data.json", function(data){
         for(var i in data){
-            $(".genreTab").append("<div>" + data[i].animal + "（" + data[i].type + "）</div>");
+          console.log(i);
+          var recruiting=$("<div class=\"recruiting\"></div>");
+          recruiting.append("<p class=\"station\">"+data[i].station+"</p>");
+          recruiting.append("<p class=\"date\">"+data[i].date+"</p>");
+
+          recruiting.append("<span class=\"role\">"+data[i].role+"</span>");
+          recruiting.append("<span class=\"age\">"+data[i].age+"</span>");
+
+          $("#genreTab0 > .wrapper > .recruitingWrap").append(recruiting);
+
+          $('.recruiting>.station, .recruiting>.date').wrapAll("<div class=\"station_date\"></div>");
+          $('.recruiting>span').wrapAll("<p class=\"conditions\"></p>");
+
+
         }
     });
 });
+
+    //カレンダー
+    var $window = $(window);
+    var $year = $('#js-year');
+    var $month = $('#js-month');
+    var $tbody = $('#js-calendar-body');
+
+    var today = new Date();
+    var currentYear = today.getFullYear(),
+        currentMonth = today.getMonth();
+
+    $window.on('load',function(){
+      calendarHeading(currentYear, currentMonth);
+      calendarBody(currentYear, currentMonth, today);
+    });
+
+    function calendarBody(year, month, today){
+      var todayYMFlag = today.getFullYear() === year && today.getMonth() === month ? true : false; // 本日の年と月が表示されるカレンダーと同じか判定
+      var startDate = new Date(year, month, 1); // その月の最初の日の情報
+      var endDate  = new Date(year, month + 1 , 0); // その月の最後の日の情報
+      var startDay = startDate.getDay();// その月の最初の日の曜日を取得
+      var endDay = endDate.getDate();// その月の最後の日の曜日を取得
+      var textSkip = true; // 日にちを埋める用のフラグ
+      var textDate = 1; // 日付(これがカウントアップされます)
+      var tableBody =''; // テーブルのHTMLを格納する変数
+      var getMonth = today.getMonth();
+      getMonth+=1;//今月の月取得
+
+      for (var row = 0; row < 6; row++){
+        var tr = '<tr>';
+
+        for (var col = 0; col < 7; col++) {
+          if (row === 0 && startDay === col){
+            textSkip = false;
+          }
+          if (textDate > endDay) {
+            textSkip = true;
+          }
+          var addClass = todayYMFlag && textDate === today.getDate() ? 'is-today' : '';
+          var textTd = textSkip ? ' ' : textDate++;
+          var td = `<td class="${addClass}" id="${textTd}">`+textTd+'</td>';
+          tr += td;
+        }
+        tr += '</tr>';
+        tableBody += tr;
+      }
+      $tbody.html(tableBody);
+
+
+      
+      $("#js-calendar-body").on("click",function(event){
+        $(".recruiting").show();
+
+        $(document).ready(function () {
+          $.getJSON("data/sample_data.json", function(data){
+        
+        // $(this).css("cursor","pointer");
+
+        console.log(getMonth + "月" + event.target.id + "日");
+        //data[i].date
+        selectDate = `${year}/${getMonth}/${event.target.id}`;
+        console.log(selectDate);
+        console.log(event.target.id);
+
+        for(var i in data){
+          if(selectDate!=data[i].date){
+            $(`.recruiting:nth(${i})`).hide();
+          }else{
+            continue;
+          }
+        }
+      });
+    });
+  });
+    
+    }
+
+    function calendarHeading(year, month){
+      $year.text(year);
+      $month.text(month + 1);
+    }
+
+
 
 });
 
--- js/tab.js
+++ js/tab.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $('#tabWrapper').each(function () {
+    $('.tabWrap').each(function () {
 
         /*変数の設定*/
         var $tabNav = $(this).find('.tabNav'),
@@ -9,29 +9,22 @@ $(function () {
 
         $tabNav.on('click', 'a', function (event) {
 
-            /*
-            デフォルトの操作を発生させない
-            aタグの遷移をさせない処理
-            */
             event.preventDefault();
             var $this = $(this);
-            //すでにactiveであれば以降の処理を中止
+
             if ($this.hasClass('active')) {
                 return;
             }
 
-            //activeになっているタブを解除
             $Anchors.removeClass('active');
-            //クリックしたタブをactiveにする
             $this.addClass('active');
-            //全てのタブを非表示
             $tab.hide();
             $($this.attr('href')).show();
+
         });
 
-        //一番目のタブを選択状態にする
         $Anchors.eq(0).trigger('click');
 
     });
-
+    
 });
